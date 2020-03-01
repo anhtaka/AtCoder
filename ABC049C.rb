1bd1d1822ec5ff6252d5109bfa53d6cfd8fec53c
anhtaka <royal.norton@gamil.com>
2020-03-02 00:59:42 +0900

    fix

--- ABC049C.rb
+++ ABC049C.rb
@@ -1,4 +1,15 @@
 # 白昼夢
+# 整数の入力
+#s = gets.chomp
+#divide = ["dream", "dreamer", "erase", "eraser"]
 
-divide = ["dream", "dreamer", "erase", "eraser"]
+def daydream(words)
+    words = words.gsub(/eraser/,"").gsub(/erase/,"").gsub(/dreamer/,"").gsub(/dream/,"")
+    puts words.length == 0 ? "YES" : 'NO'
+end
+  
+words = gets.chomp.to_s
+daydream(words)
+
+
 
